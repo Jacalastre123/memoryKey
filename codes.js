6a018d6b74bfce3b9be75afd67a8c40865ab1e2a
BrowWebs <wtran786@gmail.com>
2025-11-21 10:56:30 +1100

    Update codes.js

--- codes.js
+++ codes.js
@@ -8,6 +8,7 @@ document.addEventListener("input", () => {
         document.body.style.backgroundColor = "white";
         document.querySelectorAll("div").forEach(div => { div.style.backgroundColor = "grey"})
         document.querySelectorAll("h1").forEach(div => { div.style.color = "black"})
+        document.querySelectorAll("button").forEach(button => {button.style.border = "2px solid grey"}
     }
 
     if (codeValue === "UnHide") {
